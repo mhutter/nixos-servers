11ae4542252705ea4b4dc72bec93cdece5fc4bbf
Manuel Hutter <manuel@hutter.io>
2025-08-06 08:55:13 +0200

    nix-cache: Don't publicly expose the nix store
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- hosts/nix-cache/default.nix
+++ hosts/nix-cache/default.nix
@@ -1,4 +1,4 @@
-{ config, secrets, ... }:
+{ ... }:
 
 {
   imports = [ ../../profile/hetzner-x86 ];
@@ -10,33 +10,5 @@
       80
       443
     ];
-  };
-
-  services.nix-serve = {
-    enable = true;
-    bindAddress = "127.0.0.1";
-    openFirewall = false;
-    secretKeyFile = "/var/lib/nix-serve/secret";
-  };
-
-  services.nginx = {
-    enable = true;
-    recommendedTlsSettings = true;
-    recommendedProxySettings = true;
-    recommendedOptimisation = true;
-    recommendedGzipSettings = true;
-    recommendedBrotliSettings = true;
-
-    virtualHosts."nix-cache.mhnet.app" = {
-      locations."/".proxyPass =
-        "http://${config.services.nix-serve.bindAddress}:${toString config.services.nix-serve.port}";
-      addSSL = true;
-      enableACME = true;
-    };
-  };
-
-  security.acme = {
-    acceptTerms = true;
-    defaults.email = secrets.acmeEmail;
   };
 }
