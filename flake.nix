2f16c3d339dff6e83d0083e145e4599d94a12bbc
Manuel Hutter <manuel@hutter.io>
2025-08-06 10:34:02 +0200

    Add "empty" template for bootstrapping
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- flake.nix
+++ flake.nix
@@ -6,16 +6,26 @@
   outputs =
     { self, nixpkgs }:
     let
+      username = "mh";
       secrets = hostname: import ./secrets hostname;
     in
     {
       nixosConfigurations = {
+        "template-hcloud-x86" = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./profile/hetzner-x86 ];
+          specialArgs = {
+            inherit username;
+            secrets = { };
+          };
+        };
+
         "nix-cache" = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [ ./hosts/nix-cache ];
           specialArgs = {
+            inherit username;
             secrets = secrets "nix-cache";
-            username = "mh";
           };
         };
       };
